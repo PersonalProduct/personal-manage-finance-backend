a014d5f8dc43a8c8aaf300e0fb8aa8dad5d73a4c
Cong Chinh <126130000+JustikTran@users.noreply.github.com>
2026-01-19 13:18:05 +0700

    Revert "feat: initialize mongodb connection and core schemas"

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,26 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
-import { UsersModule } from './modules/users/users.module';
-import { WalletModule } from './modules/wallet/wallet.module';
-import { TransactionsModule } from './modules/transactions/transactions.module';
-import { CategoriesModule } from './modules/categories/categories.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
-import { AuthAccountsModule } from './modules/auth-accounts/auth-accounts.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    MongooseModule.forRoot(process.env.MONGO_URI!),
     AuthModule,
-    UsersModule,
-    WalletModule,
-    TransactionsModule,
-    CategoriesModule,
-    AuthAccountsModule
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
